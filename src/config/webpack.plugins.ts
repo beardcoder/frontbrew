631480384b324a6ac457333fb8d46bd517de6752
Markus Sommer <msommer@1drop.de>
2019-04-07 20:29:35 +0200

    [FEATURE] Clean build folders

--- src/config/webpack.plugins.ts
+++ src/config/webpack.plugins.ts
@@ -1,5 +1,6 @@
 import ExtractCssChunks from 'extract-css-chunks-webpack-plugin';
 import StyleLintPlugin from 'stylelint-webpack-plugin';
+import CleanWebpackPlugin from 'clean-webpack-plugin';
 import path from 'path';
 import webpack from 'webpack';
 
@@ -9,9 +10,12 @@ import webpack from 'webpack';
  */
 const IS_CI_BUILD = !!process.env.CI;
 const ENV = IS_CI_BUILD ? 'production' : 'development';
+const clean = process.env.CLEAN_BUILD ? new CleanWebpackPlugin() : null;
 
 const config: webpack.Configuration = {
     plugins: [
+        new webpack.ProgressPlugin(),
+        clean,
         new webpack.optimize.LimitChunkCountPlugin({
             maxChunks: 1,
         }),
